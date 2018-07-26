69f4c4dc7e914781372f149000aaa116f6d7f19b
taliars <agnosiac@gmail.com>
2018-07-26 13:01:02 +0400

    clearing MainViewModel

--- MRL/ViewModel/MainViewModel.cs
+++ MRL/ViewModel/MainViewModel.cs
@@ -13,41 +13,8 @@ using MRL.Common;
 
 namespace MRL.ViewModel
 {
-     public class MainViewModel : PropertyChangedClass
-    {
-        public ObjectInfo ObjectInfo { get; set; }
-        public Devices Devices { get; set; }
-        public ICollectionView DevicesView { get; set; }
-
-        public ICommand CommandWriteWord { get; set; }
-        public ICommand CommandAddDate { get; set; }
-        public ICommand CommandDeleteDate { get; set; }
-        public ICommand CommandAddProtocol { get; set; }
-
-        private Protocol _selectedProtocol;
-        public Protocol SelectedProtocol
-        {
-            get { return _selectedProtocol; }
-            set
-            {
-                if (_selectedProtocol != value)
-                {
-                    _selectedProtocol = value;
-                    OnPropertyChanged(new PropertyChangedEventArgs("_selectedProtocol"));
-                    DevicesView.Filter = i =>
-                    {
-                        Device device = i as Device;
-                        return device.Use.ToLower().Contains(_selectedProtocol.Species) || 
-                            device.Use.Equals("Метеорология",StringComparison.InvariantCultureIgnoreCase);
-                    };
-                }
-            }
-        }
-
-
-        public Act selectedAct { get; set; }
-        public WeatherDay selectedWeatherDay { get; set; }
-        
+    public class MainViewModel
+    {       
         public MainViewModel()
         {
 
