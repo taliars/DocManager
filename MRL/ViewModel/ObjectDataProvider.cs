05485a0283468dacee88a66b42a22eaf4d212307
taliars <agnosiac@gmail.com>
2018-07-26 17:12:42 +0400

    define new data source ObjectInfoViewModel

---
+++ MRL/ViewModel/ObjectDataProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using MRL.Model;
+
+namespace MRL.ViewModel
+{
+    public class ObjectDataProvider
+    {
+        public ObjectDataViewModel GetObjectData()
+        {
+            ObjectDataViewModel objectData = new ObjectDataViewModel
+                (new ObjectData {
+                    CustomerName = "Новый заказчик",
+                    CustomerAddress = "Адрес нового заказчика",
+                    ObjectAddress = "Адрес объекта",
+                    ObjectName = "Наименование объекта ",
+                    Measurement = "Протяженность объекта 2.0 км",
+                    Acts = new List<Act> { new Act { Name = "112пч-18х"}, new Act { Name = "112пв-18х"} },
+                    Protocols = new List<Protocol>(),
+                    Order = "Договор № 112",
+                    Purpose = "новое строительство",
+                    WeatherDays = new List<WeatherDay>(),
+                });
+
+            return objectData;
+        }
+    }
+}
